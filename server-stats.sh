eedbbd5f2d4f91fd02bdf77059610659e4720f22
mosab <mosab.awad.ahmed.alhadi@gmail.com>
2025-08-04 15:52:05 +0300

    [MOD] adding memory usage to the script

--- server-stats.sh
+++ server-stats.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "==== Server Performance Stats ===="
 
-echo -e "\n ---- CPU Usage ---"
+echo -e "\n ---- CPU Usage ----"
 top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: " 100 - $8"%"}'
+
+echo -e "\n ---- Memory Usage ----"
+free | awk ' /^Mem:/ {
+	used = $2 - $7;
+	printf("Used Memory: %s\nFree Memory: %s\nUsage Percentage: (%.2f%%)\n", used, $7, (used/$2)*100)	
+}'
